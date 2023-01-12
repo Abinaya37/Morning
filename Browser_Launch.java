b0cba2242f7c7e81de466d8b1e9b9b394632fce0
Abinaya37 <107204042+Abinaya37@users.noreply.github.com>
2023-01-12 07:47:21 +0530

    Pull

--- Browser_Launch.java
+++ Browser_Launch.java
@@ -8,6 +8,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Browser_Launch {
 
 	public static void main(String[] args) {
+		
+		//Pull the code
 
 		System.setProperty("webdriver.chrome.driver",
 				"C:\\Users\\Lenovo\\eclipse-workspace\\Selenium_Project\\Driver\\chromedriver.exe");
